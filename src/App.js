e976968009cc1b1c2ad864a90b45571152740cac
Kaiyi Sun <kaiyis1@student.unimelb.edu.au>
2022-08-21 12:39:42 +1000

    feat: setup bootstrap frontend [sc-138]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 import AppRoutes from './routes/routes';
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap Bundle JS
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const App = () => {
   return (
--- src/pages/welcome/welcome.js
+++ src/pages/welcome/welcome.js
@@ -1,11 +1,19 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 import '../../index.css';
 
 const Welcome = () => {
+  const [theme, setTheme] = useState(null);
+  const [dropdownOpen, setdropdownOpen] = useState(false);
+  const resetTheme = () => {
+    setTheme(null);
+  };
+
   return (
-    <div>
+    <div className="mb-2">
       <h1>Welcome to hyper-link</h1>
       <Link to={'/login'} id="login" className="solid-buttton">
         Login
@@ -13,6 +21,28 @@ const Welcome = () => {
       <Link to={'/signup'} id="signup" className="solid-buttton">
         Signup
       </Link>
+      {/* demo: please don't keep this forever */}
+      <div
+        className={`text-capitalize h1 mb-4 w-100 text-center text-${
+          theme ? theme.toLowerCase() : 'secondary'
+        }`}
+      >
+        {`${theme || 'Default'}`}
+      </div>
+      <div className="d-flex justify-content-center p-5">
+        <Dropdown isOpen={dropdownOpen} toggle={() => setdropdownOpen(!dropdownOpen)}>
+          <DropdownToggle color={theme} caret>
+            Dropdown
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem onClick={() => setTheme('primary')}>Primary Theme</DropdownItem>
+            <DropdownItem onClick={() => setTheme('danger')}>Danger Theme</DropdownItem>
+            <DropdownItem onClick={() => setTheme('success')}>Success Theme</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem onClick={resetTheme}>Default Theme</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </div>
   );
 };
