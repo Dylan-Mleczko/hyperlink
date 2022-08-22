a6613dbf71a19ba093049bae604f56943b59faa1
Lexi Kelsall <lexi_kelsall@me.com>
2022-08-22 22:50:18 +1000

    feat: fixed styling of nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,36 @@
-import { Container, Button, Navbar, Nav, NavbarBrand, NavLink, NavItem } from 'reactstrap';
 import './header.css';
 
 export const Header = () => {
   return (
-    <Navbar className="nav-bar">
-      <Nav>
-        <NavItem>
-          <Button className="nav-button">Log in</Button>
-        </NavItem>
-        <NavItem>
-          <Button className="nav-button">Register</Button>
-        </NavItem>
-      </Nav>
-    </Navbar>
+    <nav class="navbar navbar-expand-lg nav-bar">
+      <div class="container-fluid">
+        <a class="navbar-brand">Hyper_Link</a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse d-flex flex-row justify-content-end" id="navbarNav">
+          <nav class="navbar">
+            <form class="container-fluid justify-content-start">
+              <button class="btn nav-button me-2" type="button" to="/login">
+                Log in
+              </button>
+              <button class="btn nav-button" type="button">
+                Register
+              </button>
+            </form>
+          </nav>
+        </div>
+      </div>
+    </nav>
   );
 };
