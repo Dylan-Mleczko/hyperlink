cf0e41a8d02bc6f66f9d355807b0362866a1ab80
Lexi Kelsall <lexi_kelsall@me.com>
2022-08-29 20:16:26 +1000

    feat: work on log in page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -5,27 +5,49 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import './styles.css';
 import '../../index.css';
+import { Container } from 'reactstrap';
 
 const Loginplacehold = (props) => {
   return (
-    <>
-      <div class="row mb-3">
-        <label for="inputEmail3" class="col-sm-2 col-form-label-lg">
-          Email
-        </label>
-        <div class="col-sm-3">
-          <input type="email" class="form-control" placeholder="name@email.com.au" />
+    <div className="d-flex justify-content-center details-container">
+      <div class="d-flex justify-content-center align-items-center m-5">
+        <div>
+          <form className="w-300">
+            <div class="row mb-3">
+              <label for="inputEmail3" class="col-sm-2 col-form-label">
+                username
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="email"
+                  class="form-control form-style"
+                  id="inputEmail3"
+                  placeholder="name@email.com.au"
+                />
+              </div>
+            </div>
+            <div class="d-flex flex-row justify-content-between mb-3">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                password
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="password"
+                  class="form-control form-style"
+                  id="inputPassword3"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div class="d-flex flex-row justify-content-center">
+              <button type="submit" class="btn auth-button">
+                Sign in
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-      <div class="row mb-3">
-        <label for="inputPassword3" class="col-sm-2 col-form-label-lg">
-          Password
-        </label>
-        <div class="col-sm-3">
-          <input type="password" class="form-control" placeholder="Password" />
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
