c3593f51c91eb8540b7a2a884fe38278331f7c35
Lexi Kelsall <lexi_kelsall@me.com>
2022-09-01 13:25:40 +1000

    feat: add utilities to sign up

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -1,150 +1,183 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Link, useNavigate } from 'react-router-dom';
-// import 'date-input-polyfill';
+import axios from 'axios';
 
 import './styles.css';
 import '../../index.css';
+// import { baseDevelopmentURL } from '../../utils/constants';
+import { Header } from '../../components/Header';
 
 const SignUp = (props) => {
   const navigate = useNavigate();
+
+  const [user, setUser] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
   const formik = useFormik({
     initialValues: {
-      name: '',
+      firstName: '',
+      lastName: '',
       email: '',
-      dob: '',
       password: '',
       confirmPassword: '',
       occupation: '',
     },
     validationSchema: Yup.object({
-      name: Yup.string()
+      firstName: Yup.string()
         .min(2, 'Mininum 2 characters')
         .max(30, 'Maximum 30 characters')
-        .required('Your name is required'),
+        .required('Your first name is required'),
+      lastName: Yup.string()
+        .min(2, 'Mininum 2 characters')
+        .max(30, 'Maximum 30 characters')
+        .required('Your last name is required'),
       email: Yup.string().email('Invalid email format').required('Your email is required'),
-      dob: Yup.date()
-        .test('age', 'You must be 16 or older', function (dob) {
-          const cutoff = new Date();
-          cutoff.setFullYear(cutoff.getFullYear() - 16);
-          return dob <= cutoff;
-        })
-        .required('You must enter a date of birth'),
+
       password: Yup.string().min(8, 'Minimum 8 characters').required('You must enter a password'),
       confirmPassword: Yup.string()
         .oneOf([Yup.ref('password')], 'Password does not match')
         .required('You must enter a password'),
       occupation: Yup.string(),
     }),
-    onSubmit: (values) => {
+    onSubmit: async (values) => {
       alert(JSON.stringify(values, null, 2));
-      navigate('/gallery');
+      try {
+        const data = await axios.post(`${baseDevelopmentURL}/register`, {
+          data: {
+            userDetails: { firstName: values.firstName, lastName: values.lastName },
+            authInfo: { email: values.email, password: values.password },
+          },
+        });
+
+        setLoggedIn(true);
+        setUser(data.data.data.user);
+        navigate('/gallery', { state: { user: data.data.data.user } });
+      } catch (err) {
+        setErrorMessage(err.message);
+        setError(true);
+      }
     },
   });
-
   return (
-    <div className="details-container">
-      <main className="details-main">
-        <div className="measure">
-          <h1 className="details-title">Sign Up</h1>
-          <form onSubmit={formik.handleSubmit}>
-            <div className="mt3">
-              <label className="black">Name</label>
-              <input
-                type="text"
-                name="name"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-                className="input-box-container input-reset"
-                placeholder="Name"
-              />
-              {formik.errors.name && formik.touched.name && (
-                <p className="input-error">{formik.errors.name}</p>
-              )}
-            </div>
+    <div>
+      <Header isLoggedIn={loggedIn} />
+      <div className="d-flex justify-content-center details-container">
+        <div class="d-flex justify-content-center align-items-center m-5">
+          <div>
+            <form className="w-300" onSubmit={formik.handleSubmit}>
+              <div class="row mb-3">
+                <label class="col-sm-2 col-form-label">First Name</label>
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={formik.values.firstName}
+                    onChange={formik.handleChange}
+                    class="form-control form-style"
+                    placeholder="First Name"
+                  />
+                  {formik.errors.firstName && formik.touched.firstName && (
+                    <p className="input-error">{formik.errors.firstName}</p>
+                  )}
+                </div>
+              </div>
+              <div class="d-flex flex-row justify-content-between mb-3">
+                <label class="col-sm-2 col-form-label">Last Name</label>
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={formik.values.lastName}
+                    onChange={formik.handleChange}
+                    class="form-control form-style"
+                    placeholder="Last Name"
+                  />
+                  {formik.errors.lastName && formik.touched.lastName && (
+                    <p className="input-error">{formik.errors.lastName}</p>
+                  )}
+                </div>
+              </div>
+              <div class="d-flex flex-row justify-content-between mb-3">
+                <label class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                  <input
+                    type="email"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    class="form-control form-style"
+                    placeholder="Email"
+                  />
+                  {formik.errors.lastName && formik.touched.email && (
+                    <p className="input-error">{formik.errors.email}</p>
+                  )}
+                </div>
+              </div>
+              <div class="d-flex flex-row justify-content-between mb-3">
+                <label class="col-sm-2 col-form-label">Password</label>
+                <div class="col-sm-10">
+                  <input
+                    type="password"
+                    name="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    class="form-control form-style"
+                    placeholder="Password"
+                  />
+                  {formik.errors.password && formik.touched.password && (
+                    <p className="input-error">{formik.errors.password}</p>
+                  )}
+                </div>
+              </div>
+              <div class="d-flex flex-row justify-content-between mb-3">
+                <label class="col-sm-2 col-form-label">Confirm Password</label>
+                <div class="col-sm-10">
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    value={formik.values.confirmPassword}
+                    onChange={formik.handleChange}
+                    class="form-control form-style"
+                    placeholder="Confirm Password"
+                  />
+                  {formik.errors.confirmPassword && formik.touched.confirmPassword && (
+                    <p className="input-error">{formik.errors.confirmPassword}</p>
+                  )}
+                </div>
+              </div>
+              <div class="d-flex flex-row justify-content-between mb-3">
+                <label class="col-sm-2 col-form-label">Occupation</label>
+                <div class="col-sm-10">
+                  <input
+                    type="text"
+                    name="occupation"
+                    value={formik.values.occupation}
+                    onChange={formik.handleChange}
+                    class="form-control form-style"
+                    placeholder="Occupation"
+                  />
+                </div>
+                {error && <p className="input-error">{errorMessage}</p>}
+              </div>
 
-            <div className="mt3">
-              <label className="black">Email</label>
-              <input
-                type="email"
-                name="email"
-                value={formik.values.email}
-                onChange={formik.handleChange}
-                className="input-box-container input-reset"
-                placeholder="Email"
-              />
-              {formik.errors.email && formik.touched.email && (
-                <p className="input-error">{formik.errors.email}</p>
-              )}
-            </div>
-            <div className="mt3">
-              <label className="black">Date of Birth</label>
-              <input
-                placeholder="Date of Birth"
-                type="date"
-                name="dob"
-                value={formik.values.dob}
-                onChange={formik.handleChange}
-                className="input-box-container input-reset"
-              />
-              {formik.errors.dob && formik.touched.dob && (
-                <p className="input-error">{formik.errors.dob}</p>
-              )}
-            </div>
-            <div className="mt3">
-              <label className="black">Password</label>
-              <input
-                type="password"
-                name="password"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                className="input-box-container input-reset"
-                placeholder="Password"
-              />
-              {formik.errors.password && formik.touched.password && (
-                <p className="input-error">{formik.errors.password}</p>
-              )}
-            </div>
-            <div className="mt3">
-              <label className="black">Confirm Password</label>
-              <input
-                type="password"
-                name="confirmPassword"
-                value={formik.values.confirmPassword}
-                onChange={formik.handleChange}
-                className="input-box-container input-reset"
-                placeholder="Confirm Password"
-              />
-              {formik.errors.confirmPassword && formik.touched.confirmPassword && (
-                <p className="input-error">{formik.errors.confirmPassword}</p>
-              )}
-            </div>
-            <div className="mv3">
-              <label className="black">Occupation</label>
-              <input
-                type="text"
-                name="occupation"
-                value={formik.values.occupation}
-                onChange={formik.handleChange}
-                className="input-box-container input-reset"
-                placeholder="Occupation"
-              />
-            </div>
-            <div>
-              <button type="submit" id="signup" className="solid-buttton">
-                Sign Up
-              </button>
-              <br />
+              <div class="d-flex flex-row justify-content-center">
+                <button type="submit" class="btn auth-button" id="signup">
+                  Sign Up
+                </button>
+              </div>
               <div className="lh-copy mt3">
                 <Link to={'/login'} id="login" className="details-footer">
                   Already have an account?
                 </Link>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </main>
+      </div>
     </div>
   );
 };
---
+++ src/pages/signup/signup1.js
@@ -0,0 +1,176 @@
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import './styles.css';
+import '../../index.css';
+import { baseDevelopmentURL } from '../../utils/constants';
+import { Header } from '../../components/Header';
+
+const SignUp = (props) => {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      occupation: '',
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .min(2, 'Mininum 2 characters')
+        .max(30, 'Maximum 30 characters')
+        .required('Your first name is required'),
+      lastName: Yup.string()
+        .min(2, 'Mininum 2 characters')
+        .max(30, 'Maximum 30 characters')
+        .required('Your last name is required'),
+      email: Yup.string().email('Invalid email format').required('Your email is required'),
+
+      password: Yup.string().min(8, 'Minimum 8 characters').required('You must enter a password'),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref('password')], 'Password does not match')
+        .required('You must enter a password'),
+      occupation: Yup.string(),
+    }),
+    onSubmit: async (values) => {
+      alert(JSON.stringify(values, null, 2));
+      try {
+        const data = await axios.post(`${baseDevelopmentURL}/register`, {
+          data: {
+            userDetails: { firstName: values.firstName, lastName: values.lastName },
+            authInfo: { email: values.email, password: values.password },
+          },
+        });
+
+        setLoggedIn(true);
+        setUser(data.data.data.user);
+        navigate('/gallery', { state: { user: data.data.data.user } });
+      } catch (err) {
+        setErrorMessage(err.message);
+        setError(true);
+      }
+    },
+  });
+
+  return (
+    <div>
+      <Header isLoggedIn={loggedIn} />
+      <div className="details-container">
+        <main className="details-main">
+          <div className="measure">
+            <h1 className="details-title">Sign Up</h1>
+            <form onSubmit={formik.handleSubmit}>
+              <div className="mt3">
+                <label className="black">First Name</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formik.values.firstName}
+                  onChange={formik.handleChange}
+                  className="input-box-container input-reset"
+                  placeholder="First Name"
+                />
+                {formik.errors.firstName && formik.touched.firstName && (
+                  <p className="input-error">{formik.errors.firstName}</p>
+                )}
+              </div>
+              <div className="mt3">
+                <label className="black">Last Name</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formik.values.lastName}
+                  onChange={formik.handleChange}
+                  className="input-box-container input-reset"
+                  placeholder="Last Name"
+                />
+                {formik.errors.lastName && formik.touched.lastName && (
+                  <p className="input-error">{formik.errors.lastName}</p>
+                )}
+              </div>
+
+              <div className="mt3">
+                <label className="black">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  className="input-box-container input-reset"
+                  placeholder="Email"
+                />
+                {formik.errors.email && formik.touched.email && (
+                  <p className="input-error">{formik.errors.email}</p>
+                )}
+              </div>
+              <div className="mt3">
+                <label className="black">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  className="input-box-container input-reset"
+                  placeholder="Password"
+                />
+                {formik.errors.password && formik.touched.password && (
+                  <p className="input-error">{formik.errors.password}</p>
+                )}
+              </div>
+              <div className="mt3">
+                <label className="black">Confirm Password</label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={formik.values.confirmPassword}
+                  onChange={formik.handleChange}
+                  className="input-box-container input-reset"
+                  placeholder="Confirm Password"
+                />
+                {formik.errors.confirmPassword && formik.touched.confirmPassword && (
+                  <p className="input-error">{formik.errors.confirmPassword}</p>
+                )}
+              </div>
+              <div className="mv3">
+                <label className="black">Occupation</label>
+                <input
+                  type="text"
+                  name="occupation"
+                  value={formik.values.occupation}
+                  onChange={formik.handleChange}
+                  className="input-box-container input-reset"
+                  placeholder="Occupation"
+                />
+              </div>
+              {error && <p className="input-error">{errorMessage}</p>}
+              <div>
+                <button type="submit" id="signup" className="solid-buttton">
+                  Sign Up
+                </button>
+                <br />
+                <div className="lh-copy mt3">
+                  <Link to={'/login'} id="login" className="details-footer">
+                    Already have an account?
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
