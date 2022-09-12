e2d16863dc27cf6eb61b09fc408b4aa6466a2235
Thomas-JR <thomasrobertson24@gmail.com>
2022-09-12 23:07:26 +1000

    feat(gallery.js): improve quality of tag rendering on gallery page
    retrieve data from backend to save and eventually load the tags with the correct names

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -1,4 +1,4 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
 import { Header } from '../../components/Header';
 import { useLocation } from 'react-router-dom';
 import { SelectedTags } from '../../components/selectedTags/SelectedTags';
@@ -7,18 +7,28 @@ import { baseDevelopmentURL } from '../../utils/constants';
 
 const Gallery = () => {
   const location = useLocation();
-
-  var tags = [{ name: 'test' }, { name: 'example' }];
-
-  // var tags = (async () => {
-  //   const res = await axios.get(`${baseDevelopmentURL}/tags`, {
-  //     data: {
-  //       userID: location?.state?.user?.id,
-  //     },
-  //   });
-  //   return res.data.tags;
-  // })();
-
+  var tags = [{ name: 'hello' }, { name: 'world' }];
+  const [data, setData] = useState();
+  const [isBusy, setBusy] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      // setBusy(true);
+      const url = `${process.env.REACT_APP_API_BASE}/api/v1/endpoint/`;
+      axios
+        .get(`${baseDevelopmentURL}/collection/all`, { withCredentials: true })
+        .then((response) => {
+          setBusy(false);
+          setData(response.data.data.collections);
+          console.log(response.data.data.collections);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    if (isBusy) {
+      fetchData();
+    }
+  }, []);
   return (
     <div>
       <Header
@@ -26,6 +36,15 @@ const Gallery = () => {
         userName={location?.state?.user?.name?.first}
         token={location?.state?.user?.token}
       />
+      <div>
+        {isBusy ? (
+          <h1>Loading.....</h1>
+        ) : (
+          data.map((value) => {
+            return <p key={value._id}>{value._id}</p>;
+          })
+        )}
+      </div>
       <SelectedTags tagNames={tags}></SelectedTags>
     </div>
   );
