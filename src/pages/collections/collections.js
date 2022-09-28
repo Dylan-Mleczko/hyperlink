de20fbb15b772632ca86d25a41147e148eb8520b
Lexi Kelsall <lexi_kelsall@me.com>
2022-09-28 16:20:42 +1000

    feat: create collection table and title

--- src/pages/collections/collections.js
+++ src/pages/collections/collections.js
@@ -1,19 +1,84 @@
 import { React } from 'react';
 import { Header } from '../../components/Header';
 import { useLocation } from 'react-router-dom';
+import { Title } from '../../components/Title/Title';
 
 const Collections = () => {
   const location = useLocation();
 
   return (
     <div className="body">
-      <Header
-        isLoggedIn={true}
-        userName={location?.state?.user?.name?.first}
-        token={location?.state?.user?.token}
-      />
-      <div>
-        <h1>Collection pages</h1>
+      <Header />
+      <Title text="Collection Name" />
+      <div className="d-flex justify-content-center">
+        <div className="table-responsive">
+          <table className="table table-light table-hover table-lg align-middle">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Date</th>
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+              <tr>
+                <th>example</th>
+                <td>example</td>
+                <td>example</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
