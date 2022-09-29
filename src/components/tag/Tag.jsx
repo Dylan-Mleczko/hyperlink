e5f6f80ba36f5af75b77705ab2291cb5fe40a840
Thomas-JR <thomasrobertson24@gmail.com>
2022-09-29 13:42:29 +1000

    feat: add ability to deselect tags
    deselect tags by clicking on already selected tags

--- src/components/tag/Tag.jsx
+++ src/components/tag/Tag.jsx
@@ -4,9 +4,23 @@ import { TagStore } from '../../store/TagStore';
 export const Tag = ({ tag }) => {
   const selectedTagsStore = TagStore((state) => state.selectedTags);
   const selectTag = TagStore((state) => state.selectTag);
+  const deselectTag = TagStore((state) => state.deselectTag);
+
+  const tagClicked = () => {
+    if (selectedTagsStore.includes(tag)) {
+      deselectTag(tag);
+    } else {
+      selectTag(tag);
+    }
+    console.log('tag', tag.name, 'clicked');
+  };
 
   return (
-    <div className="tag-box" onClick={() => selectTag(tag)}>
+    <div
+      style={{ background: selectedTagsStore.includes(tag) ? '#A6B9FF' : null }}
+      className="tag-box"
+      onClick={() => tagClicked()}
+    >
       {tag.name}
       {console.log(selectedTagsStore)}
     </div>
--- src/store/TagStore.js
+++ src/store/TagStore.js
@@ -4,11 +4,12 @@ import create from 'zustand';
 const TagStore = create((set) => ({
   selectedTags: [],
   selectTag: (tag) => set((state) => ({ selectedTags: [...state.selectedTags, tag] })),
-  // addTag: () => set((state) => ({ selectedTags: [...state.selectedTags, { id: '2', text: 'world' }] })),
-  // setTags: (tags) => set((state) => ({ selectedTags: tags })),
-  // selectTag: (tagName) => set((state) => ({ selectedTags: selectedTags.push(tagName) })),
-  // might have to use index
-  // deselectTag: (tagName) => set((state) => ({ selectedTags: selectedTags.remove(tagName) })),
+  deselectTag: (tag) =>
+    set((state) => ({
+      selectedTags: state.selectedTags.filter(function (removeTag) {
+        return tag != removeTag;
+      }),
+    })),
 }));
 
 export { TagStore };
