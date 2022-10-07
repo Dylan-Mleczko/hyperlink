442ac6c87b0e3a0945875b145887ae502388a76b
Thomas-JR <thomasrobertson24@gmail.com>
2022-10-07 18:32:13 +1100

    feat: collections in gallery page

--- src/components/collection/Collection.jsx
+++ src/components/collection/Collection.jsx
@@ -2,17 +2,6 @@ import './styles.css';
 import { Tag } from '../../components/tag/Tag';
 
 export const Collection = ({ collection }) => {
-  // const tagClicked = () => {
-  //   if (selectedTagsStore.includes(tag)) {
-  //     deselectTag(tag);
-  //   } else {
-  //     selectTag(tag);
-  //   }
-  //   console.log('tag', tag.name, 'clicked');
-  // };
-
-  // return <p>{collection.name}</p>;
-
   return (
     <div>
       <button className="collection-box">
