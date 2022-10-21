b3442b571e9b43a8103ef5836c288b26e024f631
Kushal Jaiswal <jaiswalk@student.unimelb.edu.au>
2022-10-21 14:51:12 +1100

    feat: added images for home page

--- src/pages/welcome/welcome.js
+++ src/pages/welcome/welcome.js
@@ -5,7 +5,11 @@ import { Fade, LightSpeed } from 'react-reveal';
 
 import '../../index.css';
 import { Header } from '../../components/Header';
-import test from '../../assets/test.png';
+import collection from '../../assets/collection.png';
+import website from '../../assets/website.png';
+import gallery from '../../assets/gallery.png';
+import table from '../../assets/table.png';
+
 import './styles.css';
 
 const Welcome = () => {
@@ -24,7 +28,7 @@ const Welcome = () => {
           </div>
           <div className="gallery-container">
             <Fade left>
-              <img src={test} alt="gallery-page" />
+              <img src={website} className="welcome-image" alt="gallery-page" />
             </Fade>
             <LightSpeed right>
               <div>
@@ -43,18 +47,19 @@ const Welcome = () => {
               </div>
             </LightSpeed>
             <Fade right>
-              <img src={test} alt="gallery-page" />
+              <img src={collection} className="welcome-image" alt="collection-page" />
             </Fade>
           </div>
           <div className="gallery-container">
             <Fade left>
-              <img src={test} alt="gallery-page" />
+              <img src={gallery} className="welcome-image" alt="gallery-page" />
             </Fade>
             <LightSpeed right>
               <div>
-                <h1 className="heading-title font-semi-bold">Share With Others</h1>
+                <h1 className="heading-title font-semi-bold">Organise your collections</h1>
                 <p className="welcomeText">
-                  Publish your work to a vibrant community and see other people's collections
+                  You can organise your collections and favourite them as well as filter for easy
+                  access.
                 </p>
               </div>
             </LightSpeed>
@@ -62,14 +67,14 @@ const Welcome = () => {
           <div className="gallery-container">
             <LightSpeed left>
               <div>
-                <h1 className="heading-title font-semi-bold">Share With Others</h1>
+                <h1 className="heading-title font-semi-bold">View your websites</h1>
                 <p className="welcomeText">
-                  Publish your work to a vibrant community and see other people's collections
+                  All your websites are arrnaged in a tabular manner for easy access
                 </p>
               </div>
             </LightSpeed>
             <Fade right>
-              <img src={test} alt="gallery-page" />
+              <img src={table} style={{ width: '42%' }} alt="gallery-page" />
             </Fade>
           </div>
           <div className="footer-box">
